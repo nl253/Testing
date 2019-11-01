a0479741ade2a6633d9423ad063e3493b38a1402
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-11-01 15:09:23 +0000

    Simplify internal code

--- testing.go
+++ testing.go
@@ -6,8 +6,14 @@ import (
 	"testing"
 )
 
+type ctxt struct {
+	NRuns uint
+}
+
+var Ctxt = &ctxt{NRuns: 1}
+
 type Equatable interface {
-    Eq(x interface{}) bool
+	Eq(x interface{}) bool
 }
 
 type Case struct {
@@ -15,64 +21,48 @@ type Case struct {
 	F        func() interface{}
 }
 
-type unit struct {
-	name   string
-	should string
-	module string
-	cases  []Case
-}
-
-func run(module string, name string, should string, cases ...Case) func(t *testing.T) {
-	unit := unit{
-		name:   name,
-		should: should,
-		module: module,
-		cases:  cases,
-	}
-	return func(t *testing.T) {
-		var idx uint = 0
-		for _, c := range cases {
-			actual := c.F()
-			if !isEq(actual, c.Expected) {
-				t.Errorf(fmtErr(&unit, &c, actual))
-			}
-		}
-		idx++
-	}
-}
-
 func Mod(module string) func(string) func(string) func(...Case) func(*testing.T) {
 	return func(name string) func(string) func(...Case) func(*testing.T) {
 		return func(should string) func(...Case) func(*testing.T) {
 			return func(cases ...Case) func(*testing.T) {
-				return run(module, name, should, cases...)
+				return func(t *testing.T) {
+					var idx uint = 0
+					for k := uint(0); k < Ctxt.NRuns; k++ {
+						for _, c := range cases {
+							actual := c.F()
+							if !isEq(actual, c.Expected) {
+								t.Errorf(fmtErr(module, name, should, &c, actual))
+							}
+						}
+						idx++
+					}
+				}
 			}
 		}
 	}
 }
 
 func isEq(x interface{}, y interface{}) bool {
-    switch x.(type) {
-    case Equatable:
-        return x.(Equatable).Eq(y)
-    default:
-        return reflect.DeepEqual(x, y)
-    }
+	switch x.(type) {
+	case Equatable:
+		return x.(Equatable).Eq(y)
+	default:
+		return reflect.DeepEqual(x, y)
+	}
 }
-
 
 func stringify(x interface{}) string {
-    switch x.(type) {
-    case fmt.Stringer:
-        return x.(fmt.Stringer).String()
-    default:
-        return fmt.Sprintf("%v", x)
-    }
+	switch x.(type) {
+	case fmt.Stringer:
+		return x.(fmt.Stringer).String()
+	default:
+		return fmt.Sprintf("%v", x)
+	}
 }
 
-func fmtErr(unit *unit, c *Case, actual interface{}) string {
-	return fmt.Sprintf("\n\n[TEST %s.%s FAILED]\n\n" +
-	    "SHOULD   %s\n\n" +
-	    "EXPECTED %v :: %T\n" +
-	    "GOT      %v :: %T", unit.module, unit.name, unit.should, stringify(c.Expected), c.Expected, stringify(actual), actual)
+func fmtErr(module string, name string, should string, c *Case, actual interface{}) string {
+	return fmt.Sprintf("\n\n[TEST %s.%s FAILED]\n\n"+
+		"SHOULD   %s\n\n"+
+		"EXPECTED %v :: %T\n"+
+		"GOT      %v :: %T", module, name, should, stringify(c.Expected), c.Expected, stringify(actual), actual)
 }
