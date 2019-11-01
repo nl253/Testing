5f57b7d95c95a2d385fa3783c547e6f169699f7a
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-11-01 12:03:03 +0000

    Fix

--- testing.go
+++ testing.go
@@ -12,44 +12,50 @@ type TestCase struct {
 	f      func(args []interface{}) interface{}
 }
 
-type UnitTest struct {
+type unitTest struct {
 	name   string
 	should string
 	module string
-	tests  []TestCase
+	cases  []TestCase
 }
 
-func (unit *UnitTest) Run(module string, name string, should string, cases []TestCase) func(t *testing.T) {
+func Run(module string, name string, should string, cases ...TestCase) func(t *testing.T) {
+	unit := unitTest{
+		name:   name,
+		should: should,
+		module: module,
+		cases:  cases,
+	}
 	return func(t *testing.T) {
 		var idx uint = 0
 		for _, c := range cases {
 			actual := c.f(c.args)
 			if !reflect.DeepEqual(actual, c.expect) {
-				t.Errorf(unit.fmtErr(&c, idx, actual))
+				t.Errorf(fmtErr(&unit, &c, idx, actual))
 			}
 		}
 		idx++
 	}
 }
 
-func (unit *UnitTest) Mod(module string) func(name string, should string, cases []TestCase) func(t *testing.T) {
-	return func(name string, should string, cases []TestCase) func(t *testing.T) {
-		return unit.Run(module, name, should, cases)
+func Mod(module string) func(name string, should string, cases ...TestCase) func(t *testing.T) {
+	return func(name string, should string, cases ...TestCase) func(t *testing.T) {
+		return Run(module, name, should, cases...)
 	}
 }
 
-func (unit *UnitTest) Func(module string, funcName string) func(should string, cases []TestCase) func(t *testing.T) {
-	return func(should string, cases []TestCase) func(t *testing.T) {
-		return unit.Run(module, funcName, should, cases)
+func Func(module string, funcName string) func(should string, cases ...TestCase) func(t *testing.T) {
+	return func(should string, cases ...TestCase) func(t *testing.T) {
+		return Run(module, funcName, should, cases...)
 	}
 }
 
-func (unit *UnitTest) Should(module string, funcName string, should string) func(cases []TestCase) func(t *testing.T) {
-	return func(cases []TestCase) func(t *testing.T) {
-		return unit.Run(module, funcName, should, cases)
+func Should(module string, funcName string, should string) func(cases ...TestCase) func(t *testing.T) {
+	return func(cases ...TestCase) func(t *testing.T) {
+		return Run(module, funcName, should, cases...)
 	}
 }
 
-func (unit *UnitTest) fmtErr(c *TestCase, idx uint, actual interface{}) string {
+func fmtErr(unit *unitTest, c *TestCase, idx uint, actual interface{}) string {
 	return fmt.Sprintf("[TEST %s.%s FAILED] case #%d | SHOULD %s | EXPECTED %v BUT GOT %v", unit.module, unit.name, idx, unit.should, c.expect, actual)
 }
