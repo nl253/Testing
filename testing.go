2383a7823df041d1e997d15b605a2bca20ccd70d
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-11-01 14:18:24 +0000

    Improve err logging even more (2)

--- testing.go
+++ testing.go
@@ -6,6 +6,10 @@ import (
 	"testing"
 )
 
+type Equatable interface {
+    Eq(x interface{}) bool
+}
+
 type Case struct {
 	Expected interface{}
 	F        func() interface{}
@@ -29,7 +33,7 @@ func run(module string, name string, should string, cases ...Case) func(t *testi
 		var idx uint = 0
 		for _, c := range cases {
 			actual := c.F()
-			if !reflect.DeepEqual(actual, c.Expected) {
+			if !isEq(actual, c.Expected) {
 				t.Errorf(fmtErr(&unit, &c, actual))
 			}
 		}
@@ -47,6 +51,16 @@ func Mod(module string) func(string) func(string) func(...Case) func(*testing.T)
 	}
 }
 
+func isEq(x interface{}, y interface{}) bool {
+    switch x.(type) {
+    case Equatable:
+        return x.(Equatable).Eq(y)
+    default:
+        return reflect.DeepEqual(x, y)
+    }
+}
+
+
 func stringify(x interface{}) string {
     switch x.(type) {
     case fmt.Stringer:
@@ -57,5 +71,8 @@ func stringify(x interface{}) string {
 }
 
 func fmtErr(unit *unit, c *Case, actual interface{}) string {
-	return fmt.Sprintf("[TEST %s.%s FAILED] SHOULD %s | EXPECTED %v :: %T BUT GOT %v :: %T", unit.module, unit.name, unit.should, stringify(c.Expected), c.Expected, stringify(actual), actual)
+	return fmt.Sprintf("\n\n[TEST %s.%s FAILED]\n\n" +
+	    "SHOULD   %s\n\n" +
+	    "EXPECTED %v :: %T\n" +
+	    "GOT      %v :: %T", unit.module, unit.name, unit.should, stringify(c.Expected), c.Expected, stringify(actual), actual)
 }
